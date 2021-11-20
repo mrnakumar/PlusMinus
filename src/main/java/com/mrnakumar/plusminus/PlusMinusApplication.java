c2876d0fca76940f5d70d3e32b8d70fa32d469d4
Narendra Kumar <nkumar@collab.net>
2021-11-20 02:44:09 +0100

    Initial commit

---
+++ src/main/java/com/mrnakumar/plusminus/PlusMinusApplication.java
@@ -0,0 +1,13 @@
+package com.mrnakumar.plusminus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusMinusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusMinusApplication.class, args);
+    }
+
+}
